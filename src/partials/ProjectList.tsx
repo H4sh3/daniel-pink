64bb2281c66923ce660bf335a05c29081dea190f
h4sh3 <pinkdaniel@web.de>
2022-08-13 12:29:58 +0200

    add projects to start page

--- src/partials/ProjectList.tsx
+++ src/partials/ProjectList.tsx
@@ -10,57 +10,60 @@ const ProjectList = () => (
   <Section
     title={
       <>
-        Recent <GradientText>Projects</GradientText>
+        Some <GradientText>
+          <a href="/projects">
+            Projects
+          </a>
+        </GradientText>
       </>
     }
   >
     <div className="flex flex-col gap-6">
       <Project
-        name="Project 1"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-        bibendum. Nunc non posuere consectetur, justo erat semper enim, non
-        hendrerit dui odio id enim."
-        link="/"
+        name="Gymcadia - Fitness web app"
+        description="Full-stack development of a fitness app, where users can create, perform, share and track workouts."
+        link="/projects/gymcadia"
         img={{
-          src: '/assets/images/project-web-design.png',
+          src: '/assets/images/projects/gymcadia.png',
           alt: 'Project Web Design',
         }}
         category={
           <>
-            <Tags color={ColorTags.FUCHSIA}>Astro.js</Tags>
-            <Tags color={ColorTags.LIME}>Web design</Tags>
+            <Tags color={ColorTags.FUCHSIA}>Nextjs</Tags>
+            <Tags color={ColorTags.LIME}>Python</Tags>
             <Tags color={ColorTags.SKY}>Tailwind.css</Tags>
             <Tags color={ColorTags.ROSE}>TypeScript</Tags>
+            <Tags color={ColorTags.ORANGE}>Redis</Tags>
           </>
         }
       />
       <Project
-        name="Project 2"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-        bibendum. Nunc non posuere consectetur, justo erat semper enim, non
-        hendrerit dui odio id enim."
-        link="/"
-        img={{ src: '/assets/images/project-fire.png', alt: 'Project Fire' }}
+        name="Genetic Rocket"
+        description="Inspired by Space-X starship's landing maneuver. This program simulates a rocket that learns to land after a few generations of training."
+        link="/projects/genetic-rocket"
+        img={{
+          src: '/assets/images/projects/genetic-rocket.png',
+          alt: 'Project Web Design',
+        }}
         category={
           <>
-            <Tags color={ColorTags.VIOLET}>Next.js</Tags>
-            <Tags color={ColorTags.EMERALD}>Blog</Tags>
-            <Tags color={ColorTags.YELLOW}>JavaScript</Tags>
+            <Tags color={ColorTags.ROSE}>Javascript</Tags>
+            <Tags color={ColorTags.FUCHSIA}>P5.js</Tags>
           </>
         }
       />
       <Project
-        name="Project 3"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-        bibendum. Nunc non posuere consectetur, justo erat semper enim, non
-        hendrerit dui odio id enim."
-        link="/"
-        img={{ src: '/assets/images/project-maps.png', alt: 'Project Maps' }}
+        name="RL-Maze"
+        description="Implementation of an machine learning algorithm, were an agent has to find a goal in an maze."
+        link="/projects/genetic-rocket"
+        img={{
+          src: '/assets/images/projects/rl-maze.png',
+          alt: 'Project Web Design',
+        }}
         category={
           <>
-            <Tags color={ColorTags.FUCHSIA}>Astro.js</Tags>
-            <Tags color={ColorTags.INDIGO}>Bootstrap</Tags>
-            <Tags color={ColorTags.ROSE}>TypeScript</Tags>
+            <Tags color={ColorTags.ROSE}>Javascript</Tags>
+            <Tags color={ColorTags.FUCHSIA}>P5.js</Tags>
           </>
         }
       />
